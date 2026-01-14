2b5fe7320a22e120d9b27ad4fd0aa53f690c41a4
lxk2070 <lxk2070@163.com>
2026-01-14 11:51:41 +0800

    更改封面及壁纸

--- themes/anzhiyu/source/js/imgloaded.js
+++ themes/anzhiyu/source/js/imgloaded.js
@@ -59,15 +59,15 @@
 
   const ldconfig = {
     light: {
-      smallSrc: 'https://i.imgs.ovh/2025/08/02/JVe39.jpeg', //浅色模式 小图链接 尽可能配置小于100k的图片 
-      largeSrc: 'https://i.imgs.ovh/2025/08/02/JVe39.jpeg', //浅色模式 大图链接 最终显示的图片
+      smallSrc: 'https://i.imgs.ovh/2026/01/14/yrIPG1.jpeg', //浅色模式 小图链接 尽可能配置小于100k的图片 
+      largeSrc: 'https://i.imgs.ovh/2026/01/14/yrIPG1.jpeg', //浅色模式 大图链接 最终显示的图片
       mobileSmallSrc: 'https://i.imgs.ovh/2025/08/02/JVe39.jpeg', //手机端浅色小图链接 尽可能配置小于100k的图片
       mobileLargeSrc: 'https://i.imgs.ovh/2025/08/02/JVe39.jpeg', //手机端浅色大图链接 最终显示的图片
       enableRoutes: ['/'],
     },
     dark: {
-      smallSrc: 'https://i.imgs.ovh/2025/08/28/xxjz1.webp', //深色模式 小图链接 尽可能配置小于100k的图片 
-      largeSrc: 'https://i.imgs.ovh/2025/08/28/xxjz1.webp', //深色模式 大图链接 最终显示的图片
+      smallSrc: 'https://i.imgs.ovh/2026/01/14/yrIDLY.jpeg', //深色模式 小图链接 尽可能配置小于100k的图片 
+      largeSrc: 'https://i.imgs.ovh/2026/01/14/yrIDLY.jpeg', //深色模式 大图链接 最终显示的图片
       mobileSmallSrc: 'https://i.imgs.ovh/2025/08/28/xxcHn.jpeg', //手机端深色模式小图链接 尽可能配置小于100k的图片
       mobileLargeSrc: 'https://i.imgs.ovh/2025/08/28/xxcHn.jpeg', //手机端深色大图链接 最终显示的图片
       enableRoutes: ['/'],
